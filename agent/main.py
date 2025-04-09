1fd44eb5ad46eb5ce01cdec3b7c559b20d521d39
saming <1476330398@qq.com>
2025-04-09 20:37:49 +0800

    feat_启动游戏

--- agent/main.py
+++ agent/main.py
@@ -1,8 +1,9 @@
 import sys
 
 from maa.agent.agent_server import AgentServer
+from maa.custom_recognition import CustomRecognition
+from maa.custom_action import CustomAction
 from maa.toolkit import Toolkit
-
 import my_action
 import my_reco
 
@@ -15,7 +16,6 @@ def main():
     AgentServer.start_up(socket_id)
     AgentServer.join()
     AgentServer.shut_down()
-
 
 if __name__ == "__main__":
     main()
